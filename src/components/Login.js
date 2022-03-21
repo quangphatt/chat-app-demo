30b95264b80f5fdddb14f97fab5c52fb067f3485
quang_phatt <phatquang2811@gmail.com>
2022-03-21 23:00:54 +0700

    init Project

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const Login = () => {
+  return (
+    <div id='login-page'>
+        <div id='login-card'>
+            <h2>Welcome to Unichat</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Login;
